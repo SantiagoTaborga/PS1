360bbb72327598faae107b22f79f420716acdd88
SantiagoTaborga <satg123@hotmail.com>
2025-01-26 20:25:05 -0500

    Final Versions

--- PS1 Code.R
+++ PS1 Code.R
@@ -19,10 +19,11 @@ madagascar <- read_spss('Madagascar_Round9.sav') %>%
 
 tab1 <- 
   madagascar %>%
-  select(Q2, Q100, Q101, Per) %>%
+  select(Q2, Q100, Q101, Per, N) %>%
   pivot_longer(cols = -Per) %>%
   group_by(name, value) %>%
-  summarise(Percent = round(sum(Per), 2)) %>%
+  summarise(Percent = round(sum(Per), 2),
+            Number = sum(N)) %>% #test
   mutate(name = case_when(name == "Q100" ~ "Gender",
                           name == "Q101" ~ "Race",
                           name == "Q2" ~ "Language"),
@@ -37,5 +38,53 @@ tab1 <-
   
 colnames(tab1)[which(names(tab1) == "value")] <- "Value" 
 colnames(tab1)[which(names(tab1) == "name")] <- "Variable"
+tab1
 
- 
+# 3. China Attitudes -----
+## Creating the new data
+tab2 <- 
+  madagascar %>%
+  count(Q78A,name = "Number") %>%
+  mutate(Percent = round((100 * Number/sum(Number)), 2))
+
+## Finding the variable with the highest value
+
+tab2$Q78A[tab2$Percent == max(tab2$Percent)]
+
+
+## Making the table more presentable
+tab2 <- tab2 %>%
+  mutate(Q78A = case_when(Q78A == 1 ~ "Very negative",
+                          Q78A == 2 ~ "Somewhat negative",
+                          Q78A == 3 ~ "Neither positive nor negative",
+                          Q78A == 4 ~ "Somewhat positive",
+                          Q78A == 5 ~ "Very positive",
+                          Q78A == 8 ~ "Refused",
+                          Q78A == 9 ~ "Don't know",
+                          Q78A == -1 ~ "Missing"))
+
+colnames(tab2)[which(names(tab2) == "Q78A")] <- "Influence"
+## Finding the variable with the highest value part 2
+tab2$Influence[tab2$Percent == max(tab2$Percent)]
+
+## Making the table more presentable part 2
+
+tab2 <- kable(tab2, caption = "Political and Economic Influence: China")
+tab2
+
+# 4. United States attitudes ------
+## Repeated everything, just changing the variable to Q78B
+
+# 5. t-test -----
+
+madagascar <-
+  madagascar %>%
+  mutate(
+    across(
+      Q78A:Q78B,
+      ~ if_else(.x %in% 1:5, .x, NA)
+    )
+  )
+t.test(madagascar$Q78A, madagascar$Q78B, paired = TRUE)
+
+        
